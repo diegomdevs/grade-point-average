2dd8cf06ceec0684180379eb2f4fd02f24b64f4f
Diego González <diegom.develops@gmail.com>
2022-05-16 16:54:58 -0400

    feat
    [ADD] html structure, styles and JS functions

---
+++ src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class Average {
+  getAverage(grades) {
+    let gradesSum = 0;
+    for (let index = 0; index < grades.length; index++) {
+      const element = grades[index];
+      gradesSum += element;
+    }
+    return gradesSum / grades.length;
+  }
+  onClick() {
+    const grades = document.querySelector("input").value;
+    const gradesArray = grades.split(",");
+    const gradesNumberArray = [];
+    for (let index = 0; index < gradesArray.length; index++) {
+      const element = Number(gradesArray[index]);
+      gradesNumberArray.push(element);
+    }
+    const average = this.getAverage(gradesNumberArray);
+    document.getElementById("average").innerHTML = average;
+  }
+}
+const average = new Average();
